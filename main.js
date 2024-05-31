6eccf5f3b524aff332734123e1c940a6d6c76830
sehruzCode <shehruz98@mail.ru>
2024-05-31 15:16:28 +0400

    Vr 1

---
+++ main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
